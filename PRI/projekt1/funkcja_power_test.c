baf3fa2e287b2ca84636340ebd0b44b83dd55fa7
qiubix <qiubix@gmail.com>
2014-04-07 22:21:57 +0200

    PRI - add first project

---
+++ PRI/projekt1/funkcja_power_test.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+
+double mean;
+int n=4;
+
+double power(double x)
+{
+       double pot = 1.0;
+       int k;
+       for(k=1;k<n;k++)
+       {
+               pot = pot*x;
+               }
+       return pot;
+       }
+       
+       
+int main()
+{
+    double product = 1.0;
+    double x;
+    printf("Wprowadz 4 liczby:\n");
+    int j;
+    for(j=0;j<4;j++)
+    {
+            scanf("%lf",&x);
+            product *= x;
+            }
+    printf("Product to %lf\n", product);
+    mean = product;
+    double x_y;
+    x_y = power(mean);
+    
+    int i;
+    for(i=0;i<20;i++)
+    {
+            printf("%d    %lf\n",i,mean);
+                         
+            printf("%d    %lf\n",i,x_y);
+            
+            mean = ((n-1)*mean + product/x_y)/n;
+            printf("%d    %lf\n",i,mean);
+            
+            x_y = power(mean);
+            printf("potega mean %lf\n",x_y);
+            
+            printf("Koniec petli nr %d\n",i);
+            }
+    printf("Srednia to: %lf\n",mean);
+    system("pause");
+    }
---
+++ PRI/projekt1/funkcja_power_test2.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+double pot = 1.0;
+double mean;
+int n=4;
+
+double power(double x, int m)
+{
+       if(m%2==1)
+       {
+                 double b;
+                 b = x*power(x,m-1);
+                 return b;
+                 }
+       else
+       {
+           double a;
+           a = power(x,m/2);
+           return a*a;
+           }
+       }
+       
+       
+int main()
+{
+    double product = 1.0;
+    double x;
+    printf("Wprowadz 4 liczby:\n");
+    int j;
+    for(j=0;j<4;j++)
+    {
+            scanf("%lf",&x);
+            product *= x;
+            }
+    printf("Product to %lf\n", product);
+    mean = (int) product;
+    int i;
+    for(i=0;i<n;i++)
+    {
+            printf("%d    %lf\n",i,mean);
+            double x_n;
+            x_n = power(mean,n-1);
+            printf("%d    %lf\n",i,x_n);
+            double temp;
+            temp = product/x_n;
+            mean = (3*mean + temp)/n;
+            }
+    printf("Srednia to: %lf\n",mean);
+    
+    system("pause");
+    
+    
+    
+    }
+
+
+
+
