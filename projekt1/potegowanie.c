1d7aa62a146941f3b06cb7eec9dba40fd179dd5e
qiubix <qiubix@gmail.com>
2014-06-03 12:26:14 +0200

    Fix intendation in first project

--- projekt1/potegowanie.c
+++ projekt1/potegowanie.c
@@ -7,39 +7,39 @@ int n=4;
 
 double power(double x)
 {
-       double pot = 1.0;
-       int k;
-       for(k=1;k<n;k++)
-       {
-               pot = pot*x;
-               }
-       return pot;
-       }
-       
-       
+	double pot = 1.0;
+	int k;
+	for(k=1;k<n;k++)
+	{
+		pot = pot*x;
+	}
+	return pot;
+}
+
+
 int main()
 {
-    double product = 1.0;
-    double x;
-    printf("Wprowadz 4 liczby:\n");
-    int j;
-    for(j=0;j<4;j++)
-    {
-            scanf("%lf",&x);
-            product *= x;
-            }
-    printf("Product to %lf\n", product);
-    mean = product;
-    double x_y;
-    x_y = power(mean);
-    
-    int i;
-    for(i=0;i<20;i++)
-    {
-            
-            mean = ((n-1)*mean + product/x_y)/n;
-   
-            x_y = power(mean);      
-            }
-    printf("Srednia to: %lf\n",mean);
-    }
+	double product = 1.0;
+	double x;
+	printf("Wprowadz 4 liczby:\n");
+	int j;
+	for(j=0;j<4;j++)
+	{
+		scanf("%lf",&x);
+		product *= x;
+	}
+	printf("Product to %lf\n", product);
+	mean = product;
+	double x_y;
+	x_y = power(mean);
+
+	int i;
+	for(i=0;i<20;i++)
+	{
+
+		mean = ((n-1)*mean + product/x_y)/n;
+
+		x_y = power(mean);      
+	}
+	printf("Srednia to: %lf\n",mean);
+}
